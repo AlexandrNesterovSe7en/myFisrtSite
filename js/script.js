ccc87d48c7ecb9f6338a818abcbc2a61efcd4ff5
SE7EN <89002828+zxcPAPASHAzxc@users.noreply.github.com>
2022-09-11 19:55:40 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,26 +27,13 @@ changeHeightFooterDiv(heightFooter, screenWidth, divFooter)
 const footerBurgerCross = document.querySelector('.footer-burger')
 const footerFixedBurgerOpen = document.querySelector('.footer-fixed')
 
-const languageBurgerOpen = document.createElement('p')
-languageBurgerOpen.innerHTML = '<span>RU</span>|ENG'
 
 footerBurgerCross.addEventListener('click', () => {
 	footerBurgerCross.classList.toggle('cross')
 	footerFixedBurgerOpen.classList.toggle('footer-fixed__burger-open')
 	if (footerBurgerCross.matches('.cross')) {
 		document.body.style.overflow = 'hidden'
-		languageBurgerOpen.style.cssText = `
-		font-size: 34px;
-		font-weight: bold;
-		position: absolute;
-		right: 1px;
-		top: 600px;
-		transform: rotate(90deg);
-		`
-		languageBurgerOpen.querySelector('span').style.color = '#828282'
-		footerFixedBurgerOpen.append(languageBurgerOpen)
 	} else{
 		document.body.style.overflow = null
-		languageBurgerOpen.remove()
 	}
 })
