f7d2c4ce501497abc29333c4f00fc6f05bb75f9e
SE7EN <89002828+zxcPAPASHAzxc@users.noreply.github.com>
2022-09-11 20:51:35 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,21 +27,25 @@ changeHeightFooterDiv(heightFooter, screenWidth, divFooter)
 const footerBurgerCross = document.querySelector('.footer-burger')
 const footerFixedBurgerOpen = document.querySelector('.footer-fixed')
 
+function hrefCloseBurger(event) {
+	if (event.target == event.target.closest('a')){
+		footerBurgerCross.classList.toggle('cross')
+		footerFixedBurgerOpen.classList.toggle('footer-fixed__burger-open')
+		document.body.style.overflow = null
+	}
+}
+
 
 footerBurgerCross.addEventListener('click', () => {
 	footerBurgerCross.classList.toggle('cross')
 	footerFixedBurgerOpen.classList.toggle('footer-fixed__burger-open')
+	let ul = document.querySelector('.footer-menu')
 	if (footerBurgerCross.matches('.cross')) {
 		document.body.style.overflow = 'hidden'
-		let ul = document.querySelector('.footer-menu')
-		ul.addEventListener('click', (event) => {
-			if (event.target == event.target.closest('a')){
-				footerBurgerCross.classList.toggle('cross')
-				footerFixedBurgerOpen.classList.toggle('footer-fixed__burger-open')
-				document.body.style.overflow = null
-			}
-		}, {once: true})
+		ul.addEventListener('click', hrefCloseBurger, {once: true})
 	} else{
+		console.log('f')
+		ul.removeEventListener('click', hrefCloseBurger)
 		document.body.style.overflow = null
 	}
 })
