de83af8600aff16fd1fdc7b003981a9ee993a37c
SE7EN <89002828+zxcPAPASHAzxc@users.noreply.github.com>
2023-05-04 17:11:18 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,4 @@
 const screenWidth = window.screen.width;
-const divPortfolio = document.querySelector('.container-portfolio');
-
-let heightPortfolio = divPortfolio.clientHeight;
-
-const divFooter = document.querySelector('.footer')
-
-let heightFooter = divFooter.clientHeight
-
-function changeHeightPortfolioDiv(height, width, div, screenConst = 1000) {
-	if (width < 1000) {
-		let changeHeightPortfolio = height - (screenConst - width)*1.6579
-		div.style.height = `${changeHeightPortfolio}px`
-	}
-}
-
-function changeHeightFooterDiv(height, width, div, screenConst = 1000) {
-	if (width < 1000) {
-		let changeHeightPortfolio = height - (screenConst - width)*0.388
-		div.style.height = `${changeHeightPortfolio}px`
-	}
-}
-changeHeightPortfolioDiv(heightPortfolio, screenWidth, divPortfolio)
-// changeHeightFooterDiv(heightFooter, screenWidth, divFooter)
-
 
 const footerBurgerCross = document.querySelector('.footer-burger')
 const footerFixedBurgerOpen = document.querySelector('.footer-fixed')
